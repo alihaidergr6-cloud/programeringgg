5b25a74c3d1381d64749682bca386ad400898e25
Ali Haider <haider.ali1@elev.ga.ntig.se>
2025-08-27 10:46:38 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string name;
+name = "Ali" + " Haider";
+Console.WriteLine(name);
+Console.WriteLine("Hello Hows goin");
+Console.WriteLine("hello ki hal a");
+Console.WriteLine("njs");
+
+Console.ReadLine();
